c702856ecd2ab909652bec0845dbe8f7cfa83acb
HP <vaibhavsrivastava2107@gmail.com>
2020-03-18 09:26:37 +0530

    Bottom Navigation SetUp Done

---
+++ app/src/main/java/com/example/instagramhomepageclone/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.instagramhomepageclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setUpBottomNav();
+    }
+
+    private void setUpBottomNav () {
+        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNav);
+        bottomNavigationViewEx.enableAnimation(false);
+        bottomNavigationViewEx.enableItemShiftingMode(false);
+        bottomNavigationViewEx.enableShiftingMode(false);
+        bottomNavigationViewEx.setTextVisibility(false);
+    }
+}
